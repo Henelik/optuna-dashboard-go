58418ae499eb327b9f4ae227134534459c70502c
kelyn <kelyn@crandall.me>
2025-08-06 18:39:40 -0500

    add configurable port

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ import (
 
 	"github.com/Henelik/optuna-dashboard-go/pkg/db"
 	"github.com/Henelik/optuna-dashboard-go/pkg/ui"
-    "github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -15,7 +15,7 @@ import (
 
 func main() {
 	if err := godotenv.Load(); err != nil {
-		log.Fatalf("Error loading .env file: %s", err.Error())
+		log.Printf("Error loading .env file: %s", err.Error())
 	}
 
 	database, err := gorm.Open(postgres.Open(os.Getenv("OPTUNA_DB")), &gorm.Config{})
@@ -26,10 +26,13 @@ func main() {
 	db.DB = database
 
 	app := fiber.New()
-
 	app.Use(logger.New())
-
 	ui.SetupUIHandlers(app)
 
-	log.Fatal(app.Listen(":3000"))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	log.Fatal(app.Listen(":" + port))
 }
